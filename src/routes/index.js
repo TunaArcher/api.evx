14f8882a0a5a21d2ed63675d5037d87668b5b0c7
OppaKhunza <9tnk.sl@gmail.com>
2024-03-20 15:02:41 +0700

    update auth middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,19 @@
+/*==============================
+core
+==============================*/
 const express = require('express');
+
+/*==============================
+helper
+==============================*/
 const AsyncHandler = require('express-async-handler');
 const ApiResponse = require('../controllers/response/ApiResponse');
 const { StatusCodes } = require('http-status-codes');
-
-/*==============================
-Development guide:-
-  - Create separate .js file for each endpoints and import these files here (e.g. AdminRoutes.js)
-  - Wrap the controller with AsyncHandler, it will deal with try {} catch(e) {} wrapping of async program.
-
-==============================*/
 
 // importing all routes
 const router = express.Router();
 const AuthRoutes = require('./AuthRoutes');
 const UserRoutes = require('./UserRoutes');
-
-// router.use(
-//     '/',
-//     AsyncHandler(async (req, res) => {
-//         res.json(ApiResponse('Api Running Successfully.', null, StatusCodes.CREATED));
-//     })
-// );
 
 // assign prefix - to routes
 router.use('/auth', AuthRoutes);
